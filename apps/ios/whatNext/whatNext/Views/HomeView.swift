7b975cda64f8aabe45afd51576952ce7250d596f
jackkitzmiller <jkitz317@gmail.com>
2026-01-06 14:17:34 -0500

    added simple rewording to home view

--- apps/ios/whatNext/whatNext/Views/HomeView.swift
+++ apps/ios/whatNext/whatNext/Views/HomeView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack(spacing: 12) {
-            Text("whatNext").font(.title).bold()
-            Text("Next Action Here")
-                .font(.subheadline).foregroundStyle(.secondary)
+            Text("Get Recommendation").font(.title).bold()
         }
         
         .padding()
