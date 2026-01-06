6fcb0b663bf741666cff52c11833dc3ebd5f608d
Jack <jack@Graces-MacBook-Air.local>
2026-01-05 20:32:34 -0500

    added xcode project for ios frontend

---
+++ apps/ios/whatNext/whatNext/whatNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatNextApp.swift
+//  whatNext
+//
+//  Created by Jack on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct whatNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
