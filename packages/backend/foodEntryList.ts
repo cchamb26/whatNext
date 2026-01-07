3b0ba58a131b86a1c17abb7fb631ad88f463f91b
Christian Chamberland <cchamberland26@gmail.com>
2026-01-06 21:59:35 -0500

    restructure repo

--- packages/backend/foodEntryList.ts
+++
@@ -1,7 +0,0 @@
-import { foodEntry } from "./types";
-
-export async function getRecentFoodEntries(limit: number = 30 ): Promise<foodEntry[]>{
-    //once DB is setup, finalize method
-    //returns a list of <=30 entries with foodEvents.
-    return[];
-}
